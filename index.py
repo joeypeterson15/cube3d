73c3e59e4067d9e7fae07b8cdf72772bef4b178a
Joey Peterson <joey.peterson15@gmail.com>
2024-10-21 13:46:42 -0700

    projection around zaxis working. Needs yaxis/xaxis

--- index.py
+++ index.py
@@ -32,11 +32,18 @@ def main():
         lines.append(line)
     
     projectionMatrix = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, -1, -1],
-    [0, 0, 0, 0]
-]
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, -1, 0],
+        [0, 0, -1/200, 1]
+    ]
+
+    translateToOriginM = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [-xCenter, -yCenter, zCenter, 1]
+    ]
 
     translateToCenterM = [
         [1, 0, 0, 0],
@@ -53,52 +60,46 @@ def main():
 
             v = [lastx, lasty, lastZ, 1]
 
-            translateToOriginM = [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
+            rotationZaxisM = [
+                [numpy.cos(angle), -numpy.sin(angle), 0, 0],
+                [numpy.sin(angle), numpy.cos(angle), 0, 0],
                 [0, 0, 1, 0],
-                [-xCenter, -yCenter, zCenter, 1]
-            ]
-
-            # rotationZaxisM = [
-            #     [numpy.cos(angle), -numpy.sin(angle), 0, 0],
-            #     [numpy.sin(angle), numpy.cos(angle), 0, 0],
-            #     [0, 0, 1, 0],
-            #     [0, 0, 0, 1]
-            # ]
-            rotationYaxisM = [
-                [numpy.cos(angle), 0, numpy.sin(angle), 0],
-                [0, 1, 0, 0],
-                [-numpy.sin(angle), 0, numpy.cos(angle), 0],
                 [0, 0, 0, 1]
             ]
+            # rotationYaxisM = [
+            #     [numpy.cos(angle), 0, numpy.sin(angle), 0],
+            #     [0, 1, 0, 0],
+            #     [-numpy.sin(angle), 0, numpy.cos(angle), 0],
+            #     [0, 0, 0, 1]
+            # ]
             
             originV = numpy.dot(v, translateToOriginM) #translate square to the top left window origin
-            rotateV = numpy.dot(originV, rotationYaxisM) #apply rotation to square 
+            rotateV = numpy.dot(originV, rotationZaxisM) #apply rotation to square 
             projectV = numpy.dot(rotateV, projectionMatrix)
             finalV = numpy.dot(projectV, translateToCenterM) #translate back to center of window
+
+            # print('finalV', finalV)
 
             vertices[i][0] = finalV[0]
             vertices[i][1] = finalV[1]
             vertices[i][2] = finalV[2]
+            # vertices[i][0] = finalV[0] / finalV[3]
+            # vertices[i][1] = finalV[1] / finalV[3]
+            # vertices[i][2] = finalV[2] / finalV[3]
 
-            # vertices[i][0] = projectV[0]
-            # vertices[i][1] = projectV[1]
-            # vertices[i][2] = projectV[2]
-
-    def project():
-        for i in range(len(vertices)):
-            x = vertices[i][0]
-            y = vertices[i][1]
-            z = vertices[i][2]
+    # def project():
+    #     for i in range(len(vertices)):
+    #         x = vertices[i][0]
+    #         y = vertices[i][1]
+    #         z = vertices[i][2]
     
-            v = [x, y, z, 1]
+    #         v = [x, y, z, 1]
 
-            projectV = numpy.dot(v, projectionMatrix)
+    #         projectV = numpy.dot(v, projectionMatrix)
 
-            vertices[i][0] = projectV[0] / -z
-            vertices[i][1] = projectV[1] / -z
-            vertices[i][2] = projectV[2] / -z
+    #         vertices[i][0] = projectV[0] / -z
+    #         vertices[i][1] = projectV[1] / -z
+    #         vertices[i][2] = projectV[2] / -z
 
 
 
@@ -121,6 +122,6 @@ def main():
 
         if win.checkMouse():
             break
-        time.sleep(0.05)
+        time.sleep(0.03)
         
 main()
